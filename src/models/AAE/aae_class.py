a928c905dc247c904c7a7d6d23d8cc612e25d58e
Henrik Hviid Hansen <HEHHA@orsted.dk>
2020-12-22 16:09:16 +0100

    finished adversarial autoencoder class

--- src/models/AAE/aae_class.py
+++ src/models/AAE/aae_class.py
@@ -6,25 +6,39 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from src.models.AAE.aae_utils import *
 from src.models.AAE.definitions import *
-
-@dataclass()
-class AAEDataset:
-    dataset: DataFrame
-    batch_size: int
-    rows: int = field(init=False)
-    columns: int = field(init=False)
-
-    def __post_init__(self):
-        self.rows, self.columns = self.dataset.shape
-
-    def make_batches(self):
-        train_dataset = tf.data.Dataset.from_tensor_slices(self.dataset)
-        train_dataset = train_dataset.shuffle(buffer_size=self.dataset.shape[0])
-        train_dataset = train_dataset.batch(self.batch_size)
-        return train_dataset
+import time
 
 
-class AdversarialAutoencoder(AAEDataset):
+class AELossHandler:
+    def __init__(self):
+        self.autoencoder_epoch_mean_loss = tf.metrics.Mean()
+        self.discriminator_epoch_mean_loss = tf.metrics.Mean()
+        self.discriminator_epoch_mean_accuracy = tf.metrics.Mean()
+        self.generator_epoch_mean_loss = tf.metrics.Mean()
+
+    def update_losses(self,
+                      autoencoder_loss,
+                      discriminator_loss,
+                      discriminator_accuracy,
+                      generator_loss):
+        self.autoencoder_epoch_mean_loss(autoencoder_loss)
+        self.discriminator_epoch_mean_loss(discriminator_loss)
+        self.discriminator_epoch_mean_accuracy(discriminator_accuracy)
+        self.generator_epoch_mean_loss(generator_loss)
+
+    def get_losses(self):
+        """
+        Get current mean losses from all the models
+        :return: last recorded mean loss for all the models,
+        :rtype: tuple
+        """
+        return (self.autoencoder_epoch_mean_loss.result(),
+                self.discriminator_epoch_mean_loss.result(),
+                self.discriminator_epoch_mean_accuracy.result(),
+                self.generator_epoch_mean_loss.result())
+
+# TODO add validation feature to training
+class AdversarialAutoencoder:
     def __init__(self, first_hidden_layer_dimension: int, latent_layer_dimension: int):
         self.first_hidden_layer_dimension = first_hidden_layer_dimension
         self.latent_layer_dimension = latent_layer_dimension
@@ -36,6 +50,9 @@ class AdversarialAutoencoder(AAEDataset):
         self.batch_size = None
         self.number_of_features = None
         self.number_of_train_data_rows = None
+        self.autoencoder_optimizer = None
+        self.discriminator_optimizer = None
+        self.generator_optimizer = None
         # TODO architecture choices should be taken by __init__ method
 
     def _prepare_dataset_in_batches(self, df_features: DataFrame):
@@ -44,24 +61,131 @@ class AdversarialAutoencoder(AAEDataset):
         train_dataset = train_dataset.batch(self.batch_size)
         return train_dataset
 
-    def fit(self, df_features: DataFrame, batch_size: int = 128, epochs: int = 10):
-        self.number_of_train_data_rows = df_features.shape[0]
-        self.number_of_features = df_features.shape[1]
+    def _get_updated_learning_rate(self, base_learning_rate, global_step, max_learning_rate, step_size):
+        global_step = global_step + 1
+        cycle = np.floor(1 + global_step / (2 * step_size))
+        x_lr = np.abs(global_step / step_size - 2 * cycle + 1)
+        new_learning_rate = base_learning_rate + (max_learning_rate - base_learning_rate) * max(0, 1 - x_lr)
+        return new_learning_rate
+
+    def _set_learning_rates(self, learning_rate):
+        self.autoencoder_optimizer.lr = learning_rate
+        self.discriminator_optimizer.lr = learning_rate
+        self.generator_optimizer.lr = learning_rate
+
+    def _compute_losses_and_update_weights(self, batch_x):
+        def _update_model_weights(optimizer, gradients, trainable_variables):
+            optimizer.apply_gradients(zip(gradients, trainable_variables))
+
+        def _concat_tf_variables(tf_array_1, tf_array_2):
+            return tf.concat([tf_array_1, tf_array_2], axis=0)
+
+        # -------------------------------------------------------------------------------------------------------------
+        # Autoencoder
+        with tf.GradientTape() as autoencoder_tape:
+            encoder_output = self.encoder(batch_x, training=True)
+            decoder_output = self.decoder(encoder_output, training=True)
+            reconstruction_loss = self._reconstruction_loss(batch_x, decoder_output, loss_weight=1)
+
+        autoencoder_trainable_variables = self.encoder.trainable_variables + self.decoder.trainable_variables
+        autoencoder_gradients = autoencoder_tape.gradient(reconstruction_loss, autoencoder_trainable_variables)
+        _update_model_weights(self.autoencoder_optimizer, autoencoder_gradients, autoencoder_trainable_variables)
+
+        # -------------------------------------------------------------------------------------------------------------
+        # Discriminator
+        with tf.GradientTape() as discriminator_tape:
+            # TODO make this more generic, so arbitrary distributions can be given
+            real_distribution = tf.random.normal([batch_x.shape[0], self.latent_layer_dimension], mean=0.0, stddev=1.0)
+            encoder_output = self.encoder(batch_x, training=True)
+
+            discriminator_real_sample_output = self.discriminator(real_distribution, training=True)
+            discriminator_fake_sample_output = self.discriminator(encoder_output, training=True)
+
+            discriminator_loss = self._discriminator_loss(discriminator_real_sample_output,
+                                                          discriminator_fake_sample_output, loss_weight=1)
+            accuracy = tf.keras.metrics.BinaryAccuracy()
+            discriminator_true_labels = _concat_tf_variables(tf.ones_like(discriminator_real_sample_output),
+                                                             tf.zeros_like(discriminator_fake_sample_output))
+            discriminator_predicted_labels = _concat_tf_variables(discriminator_real_sample_output,
+                                                                  discriminator_fake_sample_output)
+            discriminator_accuracy = accuracy(discriminator_true_labels, discriminator_predicted_labels)
+
+        discriminator_gradients = discriminator_tape.gradient(discriminator_loss,
+                                                              self.discriminator.trainable_variables)
+        _update_model_weights(self.discriminator_optimizer, discriminator_gradients,
+                              self.discriminator.trainable_variables)
+
+        # -------------------------------------------------------------------------------------------------------------
+        # Generator (Encoder)
+        with tf.GradientTape() as generator_tape:
+            encoder_output = self.encoder(batch_x, training=True)
+            discriminator_fake_sample_output = self.discriminator(encoder_output, training=True)
+
+            generator_loss = self._generator_loss(discriminator_fake_sample_output, loss_weight=1)
+
+        generator_gradients = generator_tape.gradient(generator_loss, self.encoder.trainable_variables)
+        _update_model_weights(self.generator_optimizer, generator_gradients, self.encoder.trainable_variables)
+
+        return reconstruction_loss, discriminator_loss, discriminator_accuracy, generator_loss
+
+    def fit(self, df_features_scaled: DataFrame,
+            batch_size: int = 128,
+            epochs: int = 10,
+            base_learning_rate: float = 0.00025,
+            max_learning_rate: float = 0.0025):
+
+        self.number_of_train_data_rows = df_features_scaled.shape[0]
+        self.number_of_features = df_features_scaled.shape[1]
         self.batch_size = batch_size
         self.epochs = epochs
         self.encoder = self._make_encoder_model()
         self.decoder = self._make_decoder_model()
         self.discriminator = self._make_discriminator_model()
+        self.autoencoder_optimizer = tf.keras.optimizers.Adam(learning_rate=base_learning_rate)
+        self.discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=base_learning_rate)
+        self.generator_optimizer = tf.keras.optimizers.Adam(learning_rate=base_learning_rate)
 
-        # prepare data
-        batch_dataset = self._prepare_dataset_in_batches(df_features)
+        step_size = 2 * np.ceil(self.number_of_train_data_rows / self.batch_size)
+        global_step = 0
 
-        # fit AAE
-        self._train_step()
+        train_dataset_in_batches = self._prepare_dataset_in_batches(df_features_scaled)
+        # Training loop
+        for epoch in range(self.epochs):
+            start = time.time()
 
-    def predict(self):
-        pass
+            # Learning rate schedule
+            if epoch in [60, 100, 300]:
+                base_learning_rate = base_learning_rate / 2
+                max_learning_rate = max_learning_rate / 2
+                step_size = step_size / 2
+                print('Learning rate decreased!')
 
+            loss_handler = AELossHandler()
+            for batch_number, batch in enumerate(train_dataset_in_batches):
+                new_learning_rate = self._get_updated_learning_rate(base_learning_rate,
+                                                                    global_step,
+                                                                    max_learning_rate,
+                                                                    step_size)
+                self._set_learning_rates(new_learning_rate)
+                losses = self._compute_losses_and_update_weights(batch)
+
+                loss_handler.update_losses(*losses)
+
+            epoch_time = time.time() - start
+            print(
+                'Epoch {:3d}: Time: {:.2f}s, ETA: {:.0f}s, AE loss: {:.4f}, Discriminator loss: {:.4f}, '
+                'discriminator accuracy: {:.4f}, generator loss: {:.4f}'.format(epoch, epoch_time,
+                                                                                epoch_time * (self.epochs - epoch),
+                                                                                *loss_handler.get_losses()
+                                                                                )
+            )
+
+    def predict(self, df_features_scaled):
+        latent_space = self.encoder.predict(df_features_scaled)
+        return self.decoder.predict(latent_space)
+
+    def predict_latent_space(self, df_features_scaled):
+        return self.encoder.predict(df_features_scaled)
 
     def _make_encoder_model(self):
         inputs = tf.keras.Input(shape=(self.number_of_features,))
@@ -75,7 +199,6 @@ class AdversarialAutoencoder(AAEDataset):
         model = tf.keras.Model(inputs=inputs, outputs=encoded)
         return model
 
-
     def _make_decoder_model(self):
         encoded = tf.keras.Input(shape=(self.latent_layer_dimension,))
         x = tf.keras.layers.Dense(self.first_hidden_layer_dimension - 2)(encoded)
@@ -87,7 +210,6 @@ class AdversarialAutoencoder(AAEDataset):
         reconstruction = tf.keras.layers.Dense(self.number_of_features, activation='sigmoid')(x)
         model = tf.keras.Model(inputs=encoded, outputs=reconstruction)
         return model
-
 
     def _make_discriminator_model(self):
         encoded = tf.keras.Input(shape=(self.latent_layer_dimension,))
@@ -118,68 +240,12 @@ class AdversarialAutoencoder(AAEDataset):
         cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
         return loss_weight * cross_entropy(tf.ones_like(fake_output), fake_output)
 
-    def _train_step(self, batch_x):
-        # Define cyclic learning rate
-        base_lr = 0.00025
-        max_lr = 0.0025
-
-        n_samples = x_train.shape[0]
-        step_size = 2 * np.ceil(n_samples / batch_size)
-        global_step = 0
-
-        # -------------------------------------------------------------------------------------------------------------
-        # Define optimizers
-        ae_optimizer = tf.keras.optimizers.Adam(lr=base_lr)
-        dc_optimizer = tf.keras.optimizers.Adam(lr=base_lr)
-        gen_optimizer = tf.keras.optimizers.Adam(lr=base_lr)
-        # -------------------------------------------------------------------------------------------------------------
-        # Autoencoder
-        with tf.GradientTape() as ae_tape:
-            encoder_output = encoder(batch_x, training=True)
-            decoder_output = decoder(encoder_output, training=True)
-
-            # Autoencoder loss
-            ae_loss = autoencoder_loss(batch_x, decoder_output, ae_loss_weight)
-
-        ae_grads = ae_tape.gradient(ae_loss, encoder.trainable_variables + decoder.trainable_variables)
-        ae_optimizer.apply_gradients(zip(ae_grads, encoder.trainable_variables + decoder.trainable_variables))
-
-        # -------------------------------------------------------------------------------------------------------------
-        # Discriminator
-        with tf.GradientTape() as dc_tape:
-            real_distribution = tf.random.normal([batch_x.shape[0], z_dim], mean=0.0, stddev=1.0)
-            encoder_output = encoder(batch_x, training=True)
-
-            dc_real = discriminator(real_distribution, training=True)
-            dc_fake = discriminator(encoder_output, training=True)
-
-            # Discriminator Loss
-            dc_loss = discriminator_loss(dc_real, dc_fake, dc_loss_weight)
-
-            # Discriminator Acc
-            dc_acc = accuracy(tf.concat([tf.ones_like(dc_real), tf.zeros_like(dc_fake)], axis=0),
-                              tf.concat([dc_real, dc_fake], axis=0))
-
-        dc_grads = dc_tape.gradient(dc_loss, discriminator.trainable_variables)
-        dc_optimizer.apply_gradients(zip(dc_grads, discriminator.trainable_variables))
-
-        # -------------------------------------------------------------------------------------------------------------
-        # Generator (Encoder)
-        with tf.GradientTape() as gen_tape:
-            encoder_output = encoder(batch_x, training=True)
-            dc_fake = discriminator(encoder_output, training=True)
-
-            # Generator loss
-            gen_loss = generator_loss(dc_fake, gen_loss_weight)
-
-        gen_grads = gen_tape.gradient(gen_loss, encoder.trainable_variables)
-        gen_optimizer.apply_gradients(zip(gen_grads, encoder.trainable_variables))
-
-        return ae_loss, dc_loss, dc_acc, gen_loss
-
 
 if __name__ == "__main__":
     pd.set_option('display.max_columns', None)
     df_features = get_local_pump_data(small_dataset=True, station="SSV", component="CWP", pump_number="10")
-    dataset = AAEDataset(df_features, 256).make_batches()
 
+    aae = AdversarialAutoencoder(first_hidden_layer_dimension=12, latent_layer_dimension=2)
+    scaler = MinMaxScaler()
+    df_features_scaled = scaler.fit_transform(df_features)
+    aae.fit(df_features_scaled)
