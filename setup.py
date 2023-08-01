fcf3878df6124fc2beadaa7b29099c7a49a7ce1e
Henrik Hviid Hansen <HEHHA@orsted.dk>
2023-08-01 16:40:22 +0200

    apparently forgot to update setup file with new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="phd-argue",
-    version="1.0.6",
+    version="1.0.7",
     description="Python package with ARGUE anomaly detection model",
     author="Henrik Hviid Hansen",
     packages=find_packages(),
